cd6b8a866688fd6df9f11e4111f96402807c7fbc
Germán Katzenelson <germanka@outlook.com>
2025-05-30 20:04:29 -0300

    Rectificación con homografía

--- interfaz.py
+++ interfaz.py
@@ -6,7 +6,7 @@ def limpiar_consola():
 
 
 def seleccionar_video(carpeta="videos"):
-    # limpiar_consola()
+    limpiar_consola()
     videos = [f for f in os.listdir(carpeta) if f.lower().endswith((".mp4", ".avi", ".mov"))]
 
     if not videos:
@@ -21,7 +21,7 @@ def seleccionar_video(carpeta="videos"):
     return os.path.join(carpeta, seleccion)
 
 def elegir_roi():
-    # limpiar_consola()
+    limpiar_consola()
     opcion = questionary.select(
         "Seleccioná una opción:",
         choices=[
@@ -35,7 +35,7 @@ def elegir_roi():
 
 
 def elegir_tolerancia_frames():
-    # limpiar_consola()
+    limpiar_consola()
     opcion = questionary.select(
         "Seleccioná una opción:",
         choices=[
@@ -55,7 +55,7 @@ def elegir_tolerancia_frames():
         return None
     
 def elegir_angulo():
-    # limpiar_consola()
+    limpiar_consola()
     opcion = questionary.select(
         "Seleccioná una opción:",
         choices=[
@@ -71,6 +71,60 @@ def elegir_angulo():
         return questionary.text(
             "Ingresá el angulo en grados:"
         ).ask()
+    elif opcion[0] == "0":
+        return None
+
+def elegir_metodo_patente():
+    limpiar_consola()
+    opcion = questionary.select(
+        "Seleccioná una opción:",
+        choices=[
+            "1. Usar metodo por defecto (Pattern matching)",
+            "2. Usar metodo alternativo (El del año pasado)",
+            "0. Salir"
+        ]
+    ).ask()
+
+    if opcion[0] == "1":
+        return "pattern_matching"
+    elif opcion[0] == "2":
+        return "pattern_matching_alternativo"
+    elif opcion[0] == "0":
+        return None
+    
+def elegir_metodo_rectificacion():
+    limpiar_consola()
+    opcion = questionary.select(
+        "Seleccioná una opción:",
+        choices=[
+            "1. Usar metodo por defecto (Rectificación hardcoded)",
+            "2. Usar metodo alternativo (Rectificación de ROI)",
+            "0. Salir"
+        ]
+    ).ask()
+
+    if opcion[0] == "1":
+        return "rectificacion_hardcoded"
+    elif opcion[0] == "2":
+        return "rectificacion_roi"
+    elif opcion[0] == "0":
+        return None
+
+def calcular_homografia():
+    limpiar_consola()
+    opcion = questionary.select(
+        "Seleccioná una opción:",
+        choices=[
+            "1. Usar homografía por defecto",
+            "2. Calcular homografía",
+            "0. Salir"
+        ]
+    ).ask()
+
+    if opcion[0] == "1":
+        return True
+    elif opcion[0] == "2":
+        return False
     elif opcion[0] == "0":
         return None
 
